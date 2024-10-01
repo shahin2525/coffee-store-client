45afbffd7bd3c38ee8e44aa8760b4bd0a47896da
shahin2525 <shahin451999@gmail.com>
2024-10-01 13:46:57 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-300">Vite + React</h1>
+
+      <button className="btn btn-primary">Primary</button>
+    </>
+  );
+}
+
+export default App;
